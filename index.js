07fa69eb11e9500c86be096c2edf9f9ef1e3b152
Michelle McConville <mchelmconvl@gmail.com>
2020-11-08 18:43:41 -0600

    added questions object

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 // array of questions for user
+const fs = requirer("fs");
+const inquirer = require("inquirer");
+
+// what is diff of inquirer.prompt([{}])
 const questions = [
+  {
+    message: "Enter the name of the Repo/Project?",
+    type: "input",
+    name: "name",
+  },
+  {
+    message: "What is the link to your application?",
+    type: "input",
+    link: "appURL"
+  }
+  {
+    message: "Enter the Repo/Project GutHub URL?",
+    type: "input",
+    name: "githubURL",
+  },
+  {
+    message: "Give a brief description of the Repo/Project?",
+    type: "input",
+    name: "description",
+  },
+  {
+    message: "What does the user need to know about using the Repo/Project?",
+    type: "input",
+    name: "usage",
+  },
+  {
+    message: "Enter the installation info for the Repo/Project?",
+    type: "input",
+    name: "install",
+  },
+//   {
+//     message: "Enter screenshot?",
+//     type: "",
+//     name: "",
+//   },
+  {
+    message: "What does the user need to know about contributing to the Repo/Project?",
+    type: "input",
+    name: "contribution",
+  },
+  {
+    message: "Enter the test instructions?",
+    type: "input",
+    name: "test",
+  },
+  {
+    message: "Enter your email address.",
+    type: "input",
+    name: "email",
+  },
+  {
+    message: "Choose the license that applies to the Repo/Project?",
+    type: "list",
+    name: ["MIT", "Apache" "ISC", "GNU", "None"],
+  },
+  {
+    message: "What is the copyright year?",
+    type: "input",
+    name: "year",
+  },
+//   {
+//     message: "?",
+//     type: "",
+//     name: "",
+//   },
 
 ];
 
